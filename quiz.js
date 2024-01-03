6424a2ac8d6592eeb49f4d2a954d4fde0dacee4b
Gabiss15 <raquelgabi.nasci1815@gmail.com>
2024-01-02 21:44:20 -0300

    Novas perguntas

--- quiz.js
+++ quiz.js
@@ -6,7 +6,7 @@ const questions = [
       "B) O verdadeiro conhecimento é atingível pela razão;",
       "C) As sombras dos verdadeiros seres são o que os sentidos captam;",
       "D) A verdade essencial é conhecida através do conceito;",
-      "E) O ser humano nunca se libertará das 'prisões' a que está submetido por mais que lute e se esforce; ->"
+      "E) O ser humano nunca se libertará das 'prisões' a que está submetido por mais que lute e se esforce;"
     ],
     correctAnswer: 4
   },
@@ -16,7 +16,7 @@ const questions = [
       "A) Thomas Hobbes, John Locke, George Berkeley e Bertrand Russell.",
       "B) Francis Bacon, George Berkeley, Husserl e John Locke.",
       "C) Isaac Newton, John Locke, Bertrand Russell e Thomas Hobbes.",
-      "D) Francis Bacon, John Locke, George Berkeley e David Hume; ->"
+      "D) Francis Bacon, John Locke, George Berkeley e David Hume; "
     ],
     correctAnswer: 3
   },
@@ -27,20 +27,20 @@ const questions = [
       "B) Existem ideias advindas dos sentidos como a visão e a audição.",
       "C) Ideias de reflexão são provenientes de operações mentais.",
       "D) A mente forma ideias complexas a partir de ideias simples.",
-      "E) Existem ideias simples, que não são criadas pela mente. ->"
+      "E) Existem ideias simples, que não são criadas pela mente."
     ],
     correctAnswer: 4
   },
   {
-    question: "(UEM – Vestibular 2010 – Inverno - Adaptado) A Filosofia de Friedrich Nietzsche (1844-1900) é marcada por uma nova relação entre o racional e o irracional, na medida em que o irracional adquire validade por corresponder à necessidade de um movimento de afirmação da vida. Analise as afirmativas abaixo e marque a alternativa que apresenta as afirmações corretas:",
+    question: "(UEM – Vestibular 2010 – Inverno - Adaptado) A Filosofia de Friedrich Nietzsche (1844-1900) é marcada por uma nova relação entre o racional e o irracional, na medida em que o irracional adquire validade por corresponder à necessidade de um movimento de afirmação da vida. Analise as afirmativas abaixo e marque a alternativa que apresenta as afirmações corretas: I – Para Nietzsche, o Iluminismo não libertou os homens de seus prejuízos, mas reforçou ainda mais seus mitos, como a crença na razão e no conhecimento científico. II – O recurso metodológico proposto por Nietzsche é a genealogia, isto é, movimento teórico que recorre à gênese de um discurso, conceito ou prática, apontando suas arbitrariedades e interesses; III – Para Nietzsche, o conhecimento é fruto de um lento processo de acumulação e comprovação empírica, cuja finalidade é salvar os fenômenos; IV – Contra a moral dos aristocratas e nobres, Nietzsche defende os fracos, isto é, a moral dos escravos; V – A “vontade de potência” é a afirmação do nacional-socialismo alemão, expresso na doutrina do super-homem e no antissemitismo nietzscheano",
     options: [
-      "I – Para Nietzsche, o Iluminismo não libertou os homens de seus prejuízos, mas reforçou ainda mais seus mitos, como a crença na razão e no conhecimento científico.",
-      "II – O recurso metodológico proposto por Nietzsche é a genealogia, isto é, movimento teórico que recorre à gênese de um discurso, conceito ou prática, apontando suas arbitrariedades e interesses.",
-      "III – Para Nietzsche, o conhecimento é fruto de um lento processo de acumulação e comprovação empírica, cuja finalidade é salvar os fenômenos.",
-      "IV – Contra a moral dos aristocratas e nobres, Nietzsche defende os fracos, isto é, a moral dos escravos.",
-      "V – A “vontade de potência” é a afirmação do nacional-socialismo alemão, expresso na doutrina do super-homem e no antissemitismo nietzscheano. ->"
+      "A) Apenas I e II são corretas.",
+      "B) Somente I e V são corretas",
+      "C) São corretas I, II e III",
+      "D) As afirmativas I, II, III e IV são corretas",
+      "E) II, III e IV são corretas"
     ],
-    correctAnswer: 4
+    correctAnswer: 0
   },
   {
     question: "(ENEM 2012) Para Platão, o que havia de verdadeiro em Parmênides era que o objeto de conhecimento é um objeto de razão e não de sensação, e era preciso estabelecer uma relação entre objeto racional e objeto sensível ou material que privilegiasse o primeiro em detrimento do segundo. Lenta, mas irresistivelmente, a Doutrina das Ideias formava-se em sua mente. (ZINGANO, M. Platão e Aristóteles: o fascínio da filosofia. São Paulo: Odysseus, 2012 - adaptado) O texto faz referência à relação entre razão e sensação, um aspecto essencial da Doutrina das Ideias de Platão (427 a.C.-346 a.C.). De acordo com o texto, como Platão se situa diante dessa relação?",
@@ -49,17 +49,51 @@ const questions = [
       "B) Privilegiando os sentidos e subordinando o conhecimento a eles.",
       "C) Atendo-se à posição de Parmênides de que razão e sensação são inseparáveis.",
       "D) Afirmando que a razão é capaz de gerar conhecimento, mas a sensação não.",
-      "E) Rejeitando a posição de Parmênides de que a sensação é superior à razão. ->"
+      "E) Rejeitando a posição de Parmênides de que a sensação é superior à razão."
     ],
     correctAnswer: 4
   },
   {
     question: "(UFS – 2011) John Locke é apontado como pioneiro do materialismo moderno. Sobre o “materialismo moderno”, é CORRETO afirmar que:",
     options: [
-      "A) “Deriva as 'ideias' de que se constitui o conhecimento diretamente das sensações que se marcaram na mente [...] não cabendo assim ao pensamento nada mais, [...] que combinar, comparar e analisar essas mesmas ideias.” ->",
+      "A) “Deriva as 'ideias' de que se constitui o conhecimento diretamente das sensações que se marcaram na mente [...] não cabendo assim ao pensamento nada mais, [...] que combinar, comparar e analisar essas mesmas ideias.”",
       "B) “Todo o princípio do conhecimento material é sensorial, transponível, relativo e infinito.”",
       "C) “O valor da experiência sensível, como fator primário da elaboração cognitiva, está na possibilidade de conhecer a essência da natureza.”",
       "D) “O conhecimento deve ser introjetado a partir da experiência extrassensorial, peculiar a todo ser pensante.”"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "8- (PUCPR – 2010) Na sua obra Novum Organum, publicado em 1620, o filósofo Francis Bacon inaugura uma nova concepção de conhecimento científico. Sobre esse novo conceito de ciência, é CORRETO afirmar que:  I. Para Bacon, a nova ciência deveria continuar usando o método dedutivo, já que ele é superior ao método indutivo, pois este estaria por demais limitado aos dados estatísticos observáveis. II. Ele pretende distanciar-se da visão aristotélica de ciência como pura contemplação, como um saber em si mesmo. III. Ele busca um tipo de ciência que não privilegie o falar (a retórica), mas o fazer (as obras), ou seja, trata-se de um método de enfrentamento da natureza visando a transformar o mundo a favor do ser humano. É isso o que explica a sua frase: “Os gregos, com efeito, possuem o que é próprio das crianças: estão sempre prontos para tagarelar, mas são incapazes de gerar, pois a sua sabedoria é farta em palavras, mas estéril em obras”. (Novum Organum, Livro I, § LXXI). IV. Para Bacon, o saber científico dos gregos (entre os quais Aristóteles) é extremamente útil para a ciência moderna porque forneceria as bases para a pesquisa instrumental.",
+    options: [
+      "A) Apenas as assertivas I e II estão corretas.",
+      "B) Apenas as assertivas II e IV estão corretas.",
+      "C) Apenas as assertivas II e III estão corretas.", 
+      "D) Todas as assertivas estão corretas.", 
+      "E) Apenas a assertiva IV está correta."
+      
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: "9- (Universidade Federal de São João Del-Rei - UFSJ - Vestibular 2012/1) Sobre os ídolos preconizados por Francis Bacon, é CORRETO afirmar que:",
+    options: [
+      "A) “A consequência imediata da ação dos ídolos é a inscrição do Homem num universo de massacre e sofrimento racional-indutivo, onde o conhecimento científico se distancia da filosofia, se deteriora e se amesquinha.”",
+      "B) “Toda idolatria é forjada no hábito e na subjetividade humanos.”",
+      "C) “Os ídolos invadem a mente humana e, para derrogá-los, é necessário um esforço racional-dedutivo de análise, como bem advertiu Aristóteles.”",
+      "D) “Os ídolos da caverna são os homens enquanto indivíduos, pois cada um [...] tem uma caverna ou uma cova que intercepta e corrompe a luz da natureza.”"
+      
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "10- (CONSULPLAN) “Em si mesmo, o homem não é bom nem mau, mas, de fato, tende a ser mau. Consequentemente, o político não deve confiar no aspecto positivo do homem, mas, sim, constatar o seu aspecto negativo e agir em consequência disso. Assim, não hesitará em ser temido e a tomar as medidas necessárias para tornar-se temível. Claro, o ideal seria o de ser ao mesmo tempo amado e temido. Mas essas duas coisas são muito difíceis de ser conciliadas e, assim, deve fazer a escolha mais funcional para o governo eficaz do Estado.” Considerando a filosofia renascentista, o pensamento descrito anteriormente deve-se a:",
+    options: [
+      "A) Nicolau Maquiavel",
+      "B) Erasmo de Roterdã",
+      "C) Miguel de Cervantes",
+      "D) Miguel de Montaigne"
+      
     ],
     correctAnswer: 0
   }
